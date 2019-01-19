23324374be832b75f356cd9a1e81c5499bb9467e
Lizer1 <46378036+Lizer1@users.noreply.github.com>
2019-01-19 11:45:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -218,13 +218,6 @@ function play(guild, song) {
 }
 
 
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
-
 
 
 
