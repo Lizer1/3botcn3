0d9a049cc1c521d51e6bdff590f69f69b4e51852
Lizer1 <46378036+Lizer1@users.noreply.github.com>
2019-01-22 21:59:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -77,7 +77,7 @@ client.on('message', async msg => {
 			        .setDescription(`**الرجآء من حضرتك إختيآر رقم المقطع** :
 ${videos.map(video2 => `[**${++index} **] \`${video2.title}\``).join('\n')}`)
 
-					.setFooter("CN  - City Noisy")
+					.setFooter("CK - CITY KINGS")
 					msg.channel.sendEmbed(embed1).then(message =>{message.delete(20000)})
 					
 					try {
